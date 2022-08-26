3381e379d1dfef6f187639124bfa4c256fa10e0b
Jaroslav Vojtek <jaroslav.vojtek@lablabs.io>
2022-08-26 17:48:40 +0200

    fix: adding sa defult values

--- values.tf
+++ values.tf
@@ -1,11 +1,26 @@
 locals {
-  values_default = yamlencode({})
+  values_irsa = yamlencode({
+    "controller" : {
+        "serviceAccount" : {
+          "annotations" : {
+            "eks.amazonaws.com/role-arn" : aws_iam_role.this.arn
+          }
+        }
+      }
+    "server": {
+      "serviceAccount" : {
+          "annotations" : {
+            "eks.amazonaws.com/role-arn" : aws_iam_role.this.arn
+          }
+        }
+    }
+  })
 }
 
 data "utils_deep_merge_yaml" "values" {
   count = var.enabled ? 1 : 0
   input = compact([
-    local.values_default,
+    var.irsa_role_create ? local.values_irsa : yamlencode({}),
     var.values
   ])
 }
